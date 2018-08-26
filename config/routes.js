341dce69e239ea5ffd0692b2d1f7997723c88f1c
Chris <aqui.christine@gmail.com>
2018-08-26 17:17:58 -0400

    added fetch route

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,40 @@
-module.exports = function (router) {
+// get the scrape function from the scripts folder
+import scrape from "../scripts/scrap";
+// get the contollers
+const articleController = require("../controllers/article");
+const commentController = require("../controllers/comment");
+
+
+
+
+export default function (router) {
   // this route renders the index
   router.get("/", function(req, res){
     res.render("index");
-  })
+  });
 
   // this route rnders the saved page
   router.get("/saved", function(req,res){
     res.render("saved");
-  })
+  });
+
+//  an api route to "fetch" our data fromt he trageted site
+router.get("/api/fetch", function(req,res){
+  // scrape the cite
+  articleController.fetch(function(err,docs){
+    // check if scrape failed or no new content
+    if(!docs || docs.insertedCount ===0 ){
+      res.json({
+        message: "Sorry mate, no new post Today. Check back later"
+      });
+    } else {
+      res.json({
+        message: `Added ${docs.insertedCount} new posts.`
+      });
+    }
+  });
+});
+
+
+
 };
