005f363a52d581aea4980b2de4d1361e318a684a
Chris <aqui.christine@gmail.com>
2018-08-26 17:05:35 -0400

    added controler for the comment section

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,3 +1,32 @@
 // bring in the comments script
 import Comment from "../models/Comment";
 import makeDate from "../scripts/date";
+
+module.exports = {
+  // add all the comments related to the post
+  get: function(data, cb){
+    Comment.finc({
+      _articleId:data._id
+    },cb);
+  },
+  save: function(data, cb){
+    // create an object for the new comment
+    var newComment = {
+      _articleId:data._id,
+      data:makeDate(),
+      body: data.body
+    };
+    // next create the comment in the db
+    Comment.create(newComment, function(err,doc){
+      if(err) throw err;
+      console.log(doc);
+      cb(doc);
+    })
+  },
+  delete: function(data, cb){
+    // rmeove comments with the id
+    Comment.remove({
+      _id:data._id
+    }, cb);
+  }
+}
